f9aed554c4a3cd154eaef0a7ad4caa27348f3bcc
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-03 14:42:49 +0100

    a

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { Text } from 'react-native';
+import Home from '@/Pages/Home/Home';
+import { s } from '@/constants/App.style';
 
 export default function HomeScreen() {
 
   return (
     <>
       <SafeAreaProvider >
-          <SafeAreaView>
-              <Text>Hello</Text>
+          <SafeAreaView style={s.container}>
+              <Home/>
           </SafeAreaView>
         
       </SafeAreaProvider>
