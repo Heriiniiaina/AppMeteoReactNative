5777dc0062171ab71c4e21b24a61aba7ff63884f
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-10 16:06:35 +0100

    addNavigation

--- api/meteo.ts
+++ api/meteo.ts
@@ -11,7 +11,7 @@ export class MeteoApi{
     }
     static async fetchCity(coords: coord) {
         const response = await axios.get(`https://geocode.xyz/${coords.lat},${coords.lng}?geoit=json`);
-        const city = response.data.standard.city
+      
         return `${response.data.standard.city}, ${response.data.standard.region}`;
     }
   
---
+++ app/(tabs)/forecast.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const forecast = (props: Props) => {
+  return (
+    <View>
+      <Text>forecast</Text>
+    </View>
+  )
+}
+
+export default forecast
