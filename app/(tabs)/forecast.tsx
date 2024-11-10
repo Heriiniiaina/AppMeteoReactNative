0509ddd59120a59e9377bffcd2cb9996503e206a
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-10 18:11:42 +0100

    forecast layout

--- app/(tabs)/forecast.tsx
+++ app/(tabs)/forecast.tsx
@@ -1,17 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text,ImageBackground } from 'react-native'
 import React from 'react'
-import {useSelector} from "react-redux"
-import {RootState} from "@/redux/store"
+import { useSelector } from "react-redux"
+import { RootState } from "@/redux/store"
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { s } from '@/constants/App.style'
+import background from "@/assets/images/background.png"
+import alata from "@/assets/fonts/11.1 Alata-Regular.ttf"
 type Props = {}
 
 const forecast = (props: Props) => {
     const forecastData = useSelector((state: RootState) => state.data.data);
     console.log(forecastData)
-  return (
-    <View>
-      <Text>forecast</Text>
-    </View>
-  )
+    return (
+        <>
+        <ImageBackground source={background} style={s.imgBackground} imageStyle={s.img}>
+          <SafeAreaProvider >
+              <SafeAreaView style={s.container}>
+                    
+              </SafeAreaView>
+          </SafeAreaProvider>
+        </ImageBackground>
+         
+        </>
+
+    )
 }
 
 export default forecast
